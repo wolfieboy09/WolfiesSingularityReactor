64e87c1ea106b2a9bcb659d4395db3da778ed078
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-06-30 17:05:41 -0400

    Add Java Doc to SingularityEnergyStorage

--- src/main/java/dev/wolfieboy09/singularity/blockentity/entities/util/SingularityEnergyStorage.java
+++ src/main/java/dev/wolfieboy09/singularity/blockentity/entities/util/SingularityEnergyStorage.java
@@ -3,35 +3,53 @@ package dev.wolfieboy09.singularity.blockentity.entities.util;
 import net.minecraftforge.energy.EnergyStorage;
 
 public class SingularityEnergyStorage extends EnergyStorage {
-    public SingularityEnergyStorage(int capacity) {
-        super(capacity);
-    }
+    /**
+     * @param capacity The max capacity of the entity
+     */
+    public SingularityEnergyStorage(int capacity) { super(capacity); }
 
-    public SingularityEnergyStorage(int capacity, int maxTransfer) {
-        super(capacity, maxTransfer);
-    }
+    /**
+     * @param capacity The max capacity of the entity
+     * @param maxTransfer The maximum transfer rate
+     */
+    public SingularityEnergyStorage(int capacity, int maxTransfer) { super(capacity, maxTransfer); }
 
-    public SingularityEnergyStorage(int capacity, int maxReceive, int maxExtract) {
-        super(capacity, maxReceive, maxExtract);
-    }
+    /**
+     * @param capacity The maximum capacity of the entity
+     * @param maxReceive How much energy can be received at once
+     * @param maxExtract How much energy can exit at once
+     */
+    public SingularityEnergyStorage(int capacity, int maxReceive, int maxExtract) { super(capacity, maxReceive, maxExtract); }
 
-    public SingularityEnergyStorage(int capacity, int maxReceive, int maxExtract, int energy) {
-        super(capacity, maxReceive, maxExtract, energy);
-    }
+    /**
+     * @param capacity The max capacity of the entity
+     * @param maxReceive How much energy can be received at once
+     * @param maxExtract How much energy can exit at once
+     * @param energy How much energy it has on the start
+     */
+    public SingularityEnergyStorage(int capacity, int maxReceive, int maxExtract, int energy) { super(capacity, maxReceive, maxExtract, energy); }
 
+    /**
+     * @param energy Sets the energy
+     */
     public void setEnergy(int energy) {
         if(energy < 0)
             energy = 0;
         if(energy > this.capacity)
             energy = this.capacity;
-
         this.energy = energy;
     }
 
+    /**
+     * @param energy Amount of energy to add
+     */
     public void addEnergy(int energy) {
         setEnergy(this.energy + energy);
     }
 
+    /**
+     * @param energy Amount of energy to remove
+     */
     public void removeEnergy(int energy) {
         setEnergy(this.energy - energy);
     }
