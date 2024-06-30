cf93233bab3d0eb43e60037dda80d73f9d3bfc6c
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-06-30 09:10:49 -0400

    working on

--- src/main/java/dev/wolfieboy09/singularity/blockentity/entities/VacuumChamberBlockEntity.java
+++ src/main/java/dev/wolfieboy09/singularity/blockentity/entities/VacuumChamberBlockEntity.java
@@ -1,73 +1,126 @@
 package dev.wolfieboy09.singularity.blockentity.entities;
 
+import dev.wolfieboy09.singularity.SingularityReactor;
+import dev.wolfieboy09.singularity.blockentity.menu.VacuumChamberMenu;
 import dev.wolfieboy09.singularity.registry.EntityRegistry;
+import net.minecraft.MethodsReturnNonnullByDefault;
+import net.minecraft.client.renderer.texture.Tickable;
 import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.Tag;
 import net.minecraft.network.chat.Component;
+import net.minecraft.network.protocol.Packet;
+import net.minecraft.network.protocol.game.ClientGamePacketListener;
+import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
 import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
-import net.minecraft.world.inventory.ContainerData;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
 import net.minecraftforge.common.util.LazyOptional;
-import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.energy.EnergyStorage;
 import net.minecraftforge.items.ItemStackHandler;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class VacuumChamberBlockEntity extends BlockEntity implements MenuProvider {
-    private LazyOptional<IItemHandler> lazyItemHandler = LazyOptional.empty();
-    public final ContainerData data;
-    private int progress = 0;
-    private int maxProgress = 260;
-    private int energy = 0;
-    private int maxEnergy = 4000;
+import javax.annotation.ParametersAreNonnullByDefault;
 
-    public static class VacuumSlots {
-        public static final int INPUT_SLOT = 0;
-        public static final int OUTPUT_SLOT = 1;
+@MethodsReturnNonnullByDefault
+@ParametersAreNonnullByDefault
+public class VacuumChamberBlockEntity extends BlockEntity implements MenuProvider, Tickable {
+    private final ItemStackHandler inventory = new ItemStackHandler(2);
+    private final LazyOptional<ItemStackHandler> inventoryOptional = LazyOptional.of(() -> this.inventory);
+
+    private final EnergyStorage energy = new EnergyStorage(10000, 1000, 0, 0);
+    private final LazyOptional<EnergyStorage> energyOptional = LazyOptional.of(() -> this.energy);
+
+    public VacuumChamberBlockEntity(BlockEntityType<?> pType, BlockPos pos, BlockState blockState) {
+        super(EntityRegistry.VACUUM_CHAMBER_BLOCK_ENTITY.get(), pos, blockState);
     }
 
-    private final ItemStackHandler itemHandler = new ItemStackHandler(2) {
-        @Override protected void onContentsChanged(int slot) { setChanged(); }
-    };
+    @Override public Component getDisplayName() {return Component.translatable("block.singularity.vacuum_chamber"); }
 
-    public VacuumChamberBlockEntity(BlockEntityType<?> pType, BlockPos pPos, BlockState pBlockState) {
-        super(EntityRegistry.VACUUM_CHAMBER_BLOCK_ENTITY.get(), pPos, pBlockState);
-        this.data = new ContainerData() {
-            @Override
-            public int get(int index) {
-                return switch (index) {
-                    case 0 -> VacuumChamberBlockEntity.this.progress;
-                    case 1 -> VacuumChamberBlockEntity.this.maxProgress;
-                    case 2 -> VacuumChamberBlockEntity.this.energy;
-                    case 3 -> VacuumChamberBlockEntity.this.maxEnergy;
-                    default -> 0;
-                };
-            }
 
-            @Override
-            public void set(int index, int value) {
-                switch (index) {
-                    case 0 -> VacuumChamberBlockEntity.this.progress = value;
-                    case 1 -> VacuumChamberBlockEntity.this.maxProgress = value;
-                    case 2 -> VacuumChamberBlockEntity.this.energy = value;
-                    case 3 -> VacuumChamberBlockEntity.this.maxEnergy = value;
-                }
-            }
+    @Override
+    public void tick() {
 
-            @Override
-            public int getCount() { return 4; }
-        };
     }
 
-    @Override public @NotNull Component getDisplayName() {return Component.translatable("block.singularity.vacuum_chamber"); }
+    @Override
+    protected void saveAdditional(CompoundTag nbt) {
+        super.saveAdditional(nbt);
+        CompoundTag data = new CompoundTag();
+        data.put("Inventory", this.inventory.serializeNBT());
+        data.put("Energy", this.energy.serializeNBT());
+        nbt.put(SingularityReactor.MOD_ID, data);
+    }
+
+    @Override
+    public void invalidateCaps() {
+        super.invalidateCaps();
+        this.inventoryOptional.invalidate();
+    }
+
+    @Override
+    public void load(CompoundTag nbt) {
+        super.load(nbt);
+        CompoundTag data = nbt.getCompound(SingularityReactor.MOD_ID);
+        if (data.isEmpty()) return;
+
+        if (data.contains("Inventory", Tag.TAG_COMPOUND)) {
+            this.inventory.deserializeNBT(data.getCompound("Inventory"));
+        }
+
+        if (data.contains("Energy", Tag.TAG_INT)) {
+            this.energy.deserializeNBT(data.getCompound("Energy"));
+        }
+    }
+
+    @Override
+    public CompoundTag getUpdateTag() {
+        CompoundTag nbt = super.getUpdateTag();
+        saveAdditional(nbt);
+        return nbt;
+    }
+
+    @Nullable @Override public Packet<ClientGamePacketListener> getUpdatePacket() { return ClientboundBlockEntityDataPacket.create(this); }
+
+    @Override
+    public @NotNull <T> LazyOptional<T> getCapability(@NotNull Capability<T> cap) {
+        if (cap == ForgeCapabilities.ITEM_HANDLER) {
+            return this.inventoryOptional.cast();
+        } else if (cap == ForgeCapabilities.ENERGY) {
+            return this.energyOptional.cast();
+        } else {
+            return super.getCapability(cap);
+        }
+    }
+
+    public LazyOptional<ItemStackHandler> getInventoryOptional() {return this.inventoryOptional; }
+    public ItemStackHandler getInventory() { return this.inventory; }
+
+    public LazyOptional<EnergyStorage> getEnergyOptional() { return this.energyOptional; }
+    public EnergyStorage getEnergy() {return this.energy; }
+
+
+    private void sendUpdate() {
+        setChanged();
+        if (this.level != null) {
+            this.level.sendBlockUpdated(this.worldPosition, getBlockState(), getBlockState(), Block.UPDATE_ALL);
+        }
+    }
+
 
     @Nullable
     @Override
-    public AbstractContainerMenu createMenu(int containerId, Inventory inventory, Player player) {
-        return null;
+    public AbstractContainerMenu createMenu(int pContainerId, Inventory pPlayerInventory, Player pPlayer) {
+        return new VacuumChamberMenu(pContainerId, pPlayerInventory, pPlayer);
     }
+
+
 }
--- src/main/java/dev/wolfieboy09/singularity/blockentity/menu/VacuumChamberMenu.java
+++ src/main/java/dev/wolfieboy09/singularity/blockentity/menu/VacuumChamberMenu.java
@@ -6,36 +6,28 @@ import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.ContainerData;
+import net.minecraft.world.inventory.MenuType;
 import net.minecraft.world.inventory.SimpleContainerData;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class VacuumChamberMenu extends AbstractContainerMenu {
-    private final VacuumChamberBlockEntity blockEntity;
-    private final Level level;
-    private final ContainerData data;
+    private int containerId;
 
-    public VacuumChamberMenu(int containerId, Inventory inventory, @NotNull FriendlyByteBuf extraData) {
-        this(containerId, inventory, inventory.player.level().getBlockEntity(extraData.readBlockPos(), new SimpleContainerData(2)));
-    }
-
-    public VacuumChamberMenu(int containerId, Inventory inventory, BlockEntity entity, ContainerData data) {
-        super(ModMenuTypes.VACUUM_CHAMBER_MENU.get(), containerId);
-        checkContainerSize(inventory, 2);
-        blockEntity = ((VacuumChamberBlockEntity) entity);
-        this.level = inventory.player.level();
-        this.data = data;
+    public VacuumChamberMenu(int containerId, Inventory inventory, Player player) {
+        super(ModMenuTypes.VACUUM_CHAMBER_MENU.get(), containerId, inventory, player);
     }
 
     @Override
-    public ItemStack quickMoveStack(Player player, int index) {
+    public ItemStack quickMoveStack(Player pPlayer, int pIndex) {
         return null;
     }
 
     @Override
-    public boolean stillValid(Player player) {
+    public boolean stillValid(Player pPlayer) {
         return false;
     }
 }
