d3906ea9159cab6853d67e712a47385dc89c313a
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-07-01 19:23:04 -0400

    Add more to JEI

--- src/main/java/dev/wolfieboy09/singularity/jei/SingularityJei.java
+++ src/main/java/dev/wolfieboy09/singularity/jei/SingularityJei.java
@@ -1,13 +1,22 @@
 package dev.wolfieboy09.singularity.jei;
 
 import dev.wolfieboy09.singularity.SingularityReactor;
+import dev.wolfieboy09.singularity.blockentity.menu.ModMenuTypes;
+import dev.wolfieboy09.singularity.blockentity.menu.VacuumChamberMenu;
 import dev.wolfieboy09.singularity.blockentity.recipes.VacuumChamberRecipe;
+import dev.wolfieboy09.singularity.blockentity.screen.VacuumChamberScreen;
+import dev.wolfieboy09.singularity.jei.category.VacuumingCategory;
+import dev.wolfieboy09.singularity.registry.BlockRegistry;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.registration.IGuiHandlerRegistration;
+import mezz.jei.api.registration.IRecipeCatalystRegistration;
 import mezz.jei.api.registration.IRecipeRegistration;
+import mezz.jei.api.registration.IRecipeTransferRegistration;
 import net.minecraft.MethodsReturnNonnullByDefault;
 import net.minecraft.client.Minecraft;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 
@@ -21,5 +30,15 @@ public class SingularityJei implements IModPlugin {
     public void registerRecipes(IRecipeRegistration registration) {
         assert Minecraft.getInstance().level != null;
         VacuumChamberRecipe.gatherAllRecipes(Minecraft.getInstance().level.getRecipeManager(), registration);
+    }
+
+    @Override
+    public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
+        registration.addRecipeCatalyst(new ItemStack(BlockRegistry.VACUUM_CHAMBER.get()), VacuumingCategory.VACUUM_CHAMBER_RECIPE_TYPE);
+    }
+
+    @Override
+    public void registerRecipeTransferHandlers(IRecipeTransferRegistration registration) {
+        registration.addRecipeTransferHandler(VacuumChamberMenu.class, ModMenuTypes.VACUUM_CHAMBER_MENU.get(), VacuumingCategory.VACUUM_CHAMBER_RECIPE_TYPE, 0, 2, 2, 36);
     }
 }
