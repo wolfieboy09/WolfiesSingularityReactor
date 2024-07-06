ae3109e02ab583505c1a2a3d392f988f61d4dd18
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2024-07-06 08:18:47 -0400

    Tiers!!

--- src/main/java/dev/wolfieboy09/singularity/registry/BlockRegistry.java
+++ src/main/java/dev/wolfieboy09/singularity/registry/BlockRegistry.java
@@ -6,11 +6,11 @@ import dev.wolfieboy09.singularity.blockentity.block.FuelCell;
 import dev.wolfieboy09.singularity.blockentity.block.VacuumChamber;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -31,7 +31,7 @@ public class BlockRegistry {
     public static final RegistryObject<Block> VACUUM_CHAMBER = registerBlock("vacuum_chamber",
             () -> new VacuumChamber(BlockBehaviour.Properties.of().sound(SoundType.METAL).requiresCorrectToolForDrops().noOcclusion()));
 
-    public static final RegistryObject<Block> ELECTRIC_FURNACE = registerBlock("eletric_furnace",
+    public static final RegistryObject<Block> ELECTRIC_FURNACE = registerBlock("electric_furnace",
             () -> new ElectricFurnace(BlockBehaviour.Properties.copy(Blocks.FURNACE).sound(SoundType.METAL).requiresCorrectToolForDrops()));
 
     // registry stuff
@@ -47,6 +47,18 @@ public class BlockRegistry {
         return toReturn;
     }
 
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, int stacksTo, Rarity rarity) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn, stacksTo, rarity);
+        return toReturn;
+    }
+
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block, Rarity rarity) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn, rarity);
+        return toReturn;
+    }
+
     private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block) {
         ItemRegistry.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
     }
@@ -55,5 +67,11 @@ public class BlockRegistry {
         ItemRegistry.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().stacksTo(stacksTo)));
     }
 
-    public static void register(IEventBus eventBus) { BLOCKS.register(eventBus); }
+    private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block, int stacksTo, Rarity rarity) {
+        ItemRegistry.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().stacksTo(stacksTo).rarity(rarity)));
+    }
+
+    private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block, Rarity rarity) {
+        ItemRegistry.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties().rarity(rarity)));
+    }
 }
---
+++ src/main/java/dev/wolfieboy09/singularity/tier/Tiers.java
@@ -0,0 +1,19 @@
+package dev.wolfieboy09.singularity.tier;
+
+
+import net.minecraft.ChatFormatting;
+import net.minecraft.world.item.Rarity;
+
+public enum Tiers {
+    STARTER(Rarity.create("starter", ChatFormatting.WHITE)),
+    NEOPHYTE(Rarity.create("neophyte", ChatFormatting.DARK_GREEN)),
+    VANGUARD(Rarity.create("vanguard", ChatFormatting.YELLOW)),
+    ARCANIST(Rarity.create("arcanist", ChatFormatting.RED)),
+    PARAGON(Rarity.create("paragon", ChatFormatting.DARK_BLUE)),
+    EXEMPLAR(Rarity.create("exemplar", ChatFormatting.DARK_PURPLE));
+
+    Tiers(Rarity rarity) {}
+}
+
+
+
